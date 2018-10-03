81a92764eeca6242a357c4dbb3faf3735ff4ebba
ltisz <kinggrahamx@gmail.com>
2018-10-02 22:02:33 -0500

    djpone2

---
+++ djpone2.py
@@ -0,0 +1,22 @@
+from ircbot import *
+import os
+import random
+
+channels = ["#equestria","#death"]
+server = "irc.slashnet.org"
+nickname = "d3rpyh00v3z"
+pw = ""
+
+irc = IRCBot()
+irc.connect(server,channels,nickname,pw)
+
+while True:
+    text = irc.get_text()
+    print(text)
+    for chan in channels:
+        if chan in text:
+            channel = chan 
+    if "PRIVMSG" in text and channel in text and "hello" in text:
+        irc.send(channel, "Hello!")
+    if "PRIVMSG" in text and channel in text and ":N" in text:
+        irc.send(channel, "I")
